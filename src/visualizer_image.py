1f9270a073d33b8cc76b3d3f929ccafe1916544b
Nicolas Ronvel <gulix33xp@yahoo.fr>
2024-06-16 21:06:33 +0200

    Added a ColorManager
    Fix #4 by adding a very basic color manager that will be expanded

--- src/visualizer_image.py
+++ src/visualizer_image.py
@@ -1,15 +1,13 @@
 from mazePlane import *
+from colors_manager import colorManager
 from PIL import Image, ImageDraw
 
-def _get_image_from_maze(maze: mazePlane, colors: list, bg_color: tuple = (0, 0, 0), cell_size=10, path_size=6):
+def _get_image_from_maze(maze: mazePlane, colors:colorManager , bg_color: tuple = (0, 0, 0), cell_size=10, path_size=6):
     """Generates a PIL image object from a mazePlane"""
     # Initialization of the PIL image object 
     im = Image.new('RGB', (maze.x_size * cell_size, maze.y_size * cell_size), bg_color)
     draw = ImageDraw.Draw(im)
 
-    # We'll iterate through the colors list for each path
-    color_index = 0
-    
     # Size of the elements
     margin_size = (cell_size - path_size) / 2
 
@@ -17,7 +15,7 @@ def _get_image_from_maze(maze: mazePlane, colors: list, bg_color: tuple = (0, 0,
     for path in maze.paths:
 
         # Getting the colors        
-        color = colors[color_index]
+        color = colors.get_next_color()
         
         previous_x = -1
         previous_y = -1
@@ -28,8 +26,8 @@ def _get_image_from_maze(maze: mazePlane, colors: list, bg_color: tuple = (0, 0,
             y = positions[1]
             draw_x = cell_size * x + margin_size
             draw_y = cell_size * y + margin_size
-            draw_end_x = cell_size * x + margin_size + path_size
-            draw_end_y = cell_size * y + margin_size + path_size
+            draw_end_x = cell_size * x + margin_size + path_size - 1 # -1 because inclusive bounds
+            draw_end_y = cell_size * y + margin_size + path_size - 1
             
             # Liaisons with the previous position
             if (x == previous_x):
@@ -48,14 +46,9 @@ def _get_image_from_maze(maze: mazePlane, colors: list, bg_color: tuple = (0, 0,
             # Drawing the rectangle for the position
             draw.rectangle((draw_x, draw_y, draw_end_x, draw_end_y), fill=color)
 
-        # Getting the new color, looping when all colors have been used
-        color_index += 1
-        if color_index >= len(colors):
-            color_index = 0
-
     return im
 
-def draw_maze(maze, image_filename = 'ofelia_maze.jpg', expand_maze = True, colors = [ (255, 0, 0) ], \
+def draw_maze(maze, colors, image_filename = 'ofelia_maze.jpg', expand_maze = True, \
               cell_size = 10, path_size = 6):
     """Draw a maze into an image file"""
     
@@ -67,9 +60,8 @@ def draw_maze(maze, image_filename = 'ofelia_maze.jpg', expand_maze = True, colo
     pil_image = _get_image_from_maze(maze, colors=colors, cell_size=cell_size, path_size=path_size)
     pil_image.save(image_filename, quality=95)
 
-def draw_maze_gif(maze:mazePlane, image_filename = 'ofelia_maze_animated.gif', \
-                  frame_duration:int = 30, loop:int = 0,\
-                    colors = [ (169, 13, 42)] ):
+def draw_maze_gif(maze:mazePlane, colors, image_filename = 'ofelia_maze_animated.gif', \
+                  frame_duration:int = 30, loop:int = 0 ):
             
     image_frames = [ ]
     image_frames.append(_get_image_from_maze(maze, colors=colors))
